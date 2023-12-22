dcbbf654aca79aa6e46d68feb92d0fbbf37afa88
GuiCT <gtomiasi@gmail.com>
2023-12-22 02:31:12 -0300

    Day 7: "Fix" first, solve second challenge

--- day-7/second/main.jl
+++ day-7/second/main.jl
@@ -5,7 +5,6 @@ char_values = Dict(
   'A' => 13,
   'K' => 12,
   'Q' => 11,
-  'J' => 10,
   'T' => 9,
   '9' => 8,
   '8' => 7,
@@ -15,6 +14,8 @@ char_values = Dict(
   '4' => 3,
   '3' => 2,
   '2' => 1,
+  # Lowest value now!
+  'J' => 0,
 )
 
 function get_card_type(card_string::String)
@@ -26,6 +27,23 @@ function get_card_type(card_string::String)
       char_dict[char] = 1
     end
   end
+  joker_count = if haskey(char_dict, 'J')
+    char_dict['J']
+  else
+    0
+  end
+  # If we have jokers
+  if joker_count > 0
+    # And there is not only jokers
+    if length(char_dict) > 1
+      # Eliminate the joker_count from the dictionary
+      pop!(char_dict, 'J')
+      # Increment it on the highest count char that is not a joker
+      highest_count_char = findmax(char_dict)[2]
+      char_dict[highest_count_char] += joker_count
+    end
+  end
+  # Proceed as usual
   char_dict_entry_count = length(char_dict)
 
   if char_dict_entry_count == 1
@@ -52,7 +70,7 @@ function get_card_type(card_string::String)
 end
 
 # Read every line
-lines = readlines("input.txt")
+lines = readlines("day-7/input.txt")
 line_count = length(lines)
 matrix_of_cards = zeros(Int64, line_count, 7)
 # For each matrix row
@@ -70,8 +88,12 @@ for (i, line) in enumerate(lines)
   matrix_of_cards[i, 7] = parse(Int64, bid)
 end
 
+matrix_of_cards
+
 # Stable sort, column by column (ignoring bid value)
 matrix_of_cards = sortslices(matrix_of_cards, dims=1, by=(x -> tuple(x[1:end-1]...)));
+
+matrix_of_cards
 # Dot product to calculate the result
 last_column = matrix_of_cards[:, end];
 ranks = 1:line_count;
